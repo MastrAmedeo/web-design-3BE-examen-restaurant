b36b10f257ea1eb77a1c017c734c02af20e634ee
Amedeo <23046@ecam.be>
2025-11-20 14:01:24 +0100

    parallax_1 sur page vin + changement nom dossiers photos

--- app/huile/page.tsx
+++ app/huile/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
   <>
-    <a href = "photos huile/287A8283.jpg">
-      <img src="photos huile/287A8283.jpg" alt="photo recolte des olives"/>
+    <a href = "photos_huile/287A8283.jpg">
+      <img src="photos_huile/287A8283.jpg" alt="photo recolte des olives"/>
     </a>
     <h1>Notre huile</h1>
   </>      
--- app/vins/page.tsx
+++ app/vins/page.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <>
-    <a href = "photos vin/_87A2399.jpg">
-      <img src="photos vin/_87A2399.jpg" alt="photo vigne mûre"/>
-    </a>
-    <h1>Notre Vin</h1>
-  </>      
+  <div className="bg-[url(/photos_vin/_87A2399.jpg)] bg-fixed bg-cover bg-center min-h-200">
+    <div className="pt-150">
+      <h1 className="bg-white py-3 rounded shadow-md">
+          Notre Vin
+      </h1>
+    </div>
+  </div>      
   );
 }
