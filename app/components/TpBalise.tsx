9ca10edcb5c5b5723967b73c7a4587b04b947907
Amedeo <23046@ecam.be>
2025-12-01 10:52:58 +0100

    slide bouton + maj Acceuil

--- app/components/TpBalise.tsx
+++ app/components/TpBalise.tsx
@@ -8,7 +8,7 @@ type BaliseProps = {
 
 export default function TpBalise(props: BaliseProps) {
     return (
-        <Link href = {props.href} className="inline-flex items-center rounded-md bg-[#313727]/80 px-5 py-3 text-m font-medium bg-[#21231d] inset-ring inset-ring-gray-400/20">
+        <Link href = {props.href} className="inline-flex items-center rounded-md bg-[#313727]/80 px-5 py-3 text-m font-medium inset-ring inset-ring-gray-400/20">
             {props.children}
         </Link>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
 import Parallax from "./components/parallax"
+import TpBalise from "./components/TpBalise";
 
 export default function Home() {
   return (
         <>
-        <div className="pt-150">
-          <h1><Parallax>Terre del Cilento</Parallax></h1>
+        <div className="bg-[url(/photos_huile/287A8560.jpg)] bg-fixed bg-cover bg-center">
+          <div className="pt-150">
+            <h1 className = "text-center"><Parallax><TpBalise href = "#section-Acceuil">Acceuil
+            </TpBalise></Parallax></h1>
+          </div>
+          <div className="bg-[#697851] min-h-screen pt-10">
+            <p className="" id = "section-Acceuil">
+              Soyez les binevenus chez Willie Wonka.
+            </p>
+          </div>
         </div>
         </>
   );
